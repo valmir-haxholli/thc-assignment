0e58c369596c7d1c8654d443badc2c01b9d2e370
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-01-17 23:48:22 +0100

    Sidebar component and styles implemented
    Sidebar component and styles implemented

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import Navbar from '../Navbar/Navbar';
+import Sidebar from '../Sidebar/Sidebar';
 
 import './Dashboard.css'
 
@@ -6,7 +7,7 @@ const Dashboard = () => (
     <div className='dashboard'>
         <Navbar />
         <div className='dashboard__content'>
-
+            <Sidebar />
         </div>
     </div>
 )
