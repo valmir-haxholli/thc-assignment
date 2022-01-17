69e36f36115716aef0cd8d8ae19a70568c1efac4
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-01-17 22:28:57 +0100

    User logout button functionality implemented
    User logout button functionality implemented

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom';
+
 import './Navbar.css'
 
 import Button from '../Button/Button';
@@ -7,32 +9,45 @@ import { ReactComponent as Users } from '../../assets/images/users.svg';
 import { ReactComponent as Chat } from '../../assets/images/chat.svg';
 import { ReactComponent as Search } from '../../assets/images/search.svg';
 
-const Navbar = () => (
-    <div className='navbar'>
-        <div className='navbar__sections'>
-            <Logo className='navbar__home' />
-            <div className='navbar__logos'>
-                <div className='navbar__logo'>
-                    <Users />
-                    <p>Network</p>
-                </div>
-                <div className='navbar__logo'>
-                    <div className='notification'>
-                        <span className='notification__content'></span>
-                        <Chat />
+const Navbar = () => {
+
+    const history = useHistory();
+
+    const userLogout = (e) => {
+        e.preventDefault();
+        sessionStorage.removeItem('authentication_token');
+        history.push('/');
+    }
+
+    return (
+        <div className='navbar'>
+            <div className='navbar__sections'>
+                <Logo className='navbar__home' />
+                <div className='navbar__logos'>
+                    <div className='navbar__logo'>
+                        <Users />
+                        <p className='logo--active'>Network</p>
                     </div>
-                    <p>Chat</p>
+                    <div className='navbar__logo'>
+                        <div className='notification'>
+                            <span className='notification__content'></span>
+                            <Chat />
+                        </div>
+                        <p>Chat</p>
+                    </div>
+                </div>
+                <div className='navbar__search'>
+                    <Search />
+                    <input type="text"  placeholder='Search'/>
                 </div>
             </div>
-            <div className='navbar__search'>
-                <Search />
-                <input type="text"  placeholder='Search'/>
-            </div>
+            <form className='btn--navbar' onSubmit={userLogout}>
+                    <Button color='primary' name='Log Out' />
+            </form>
         </div>
-        <div className='btn--navbar'>
-                <Button name="Log Out" color="primary" />
-        </div>
-    </div>
-)
+    )
+}
+    
+
 
 export default Navbar;
