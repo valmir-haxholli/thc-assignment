a52a499ba037d32d961863a36926f3256a251356
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-01-17 21:47:33 +0100

    Added Navbar Component for the app dashboard
    Added Navbar Component for the app dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,14 @@
+import Navbar from '../Navbar/Navbar';
+
 import './Dashboard.css'
 
 const Dashboard = () => (
-    <h2>Dashboard</h2>
+    <div className='dashboard'>
+        <Navbar />
+        <div className='dashboard__content'>
+
+        </div>
+    </div>
 )
 
 export default Dashboard;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import './Navbar.css'
+
+import Button from '../Button/Button';
+
+import { ReactComponent as Logo } from '../../assets/images/miniLogo.svg';
+import { ReactComponent as Users } from '../../assets/images/users.svg';
+import { ReactComponent as Chat } from '../../assets/images/chat.svg';
+import { ReactComponent as Search } from '../../assets/images/search.svg';
+
+const Navbar = () => (
+    <div className='navbar'>
+        <div className='navbar__sections'>
+            <Logo className='navbar__home' />
+            <div className='navbar__logos'>
+                <div className='navbar__logo'>
+                    <Users />
+                    <p>Network</p>
+                </div>
+                <div className='navbar__logo'>
+                    <Chat />
+                    <p>Chat</p>
+                </div>
+            </div>
+            <div className='navbar__search'>
+                <Search />
+                <input type="text"  placeholder='Search'/>
+            </div>
+        </div>
+        <div className='btn--navbar'>
+                <Button name="Log Out" color="primary" />
+        </div>
+    </div>
+)
+
+export default Navbar;
