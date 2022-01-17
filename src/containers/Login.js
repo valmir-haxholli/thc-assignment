1ff4acb199c3e2e6ea5cbdaf6c5ed92bef51ab9e
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-01-17 13:48:26 +0100

    Delete Login container
    Since we are using selectors we don't need mapStateToProps functions

--- src/containers/Login.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux';
-import LoginForm from '../components/LoginForm/LoginForm';
-import { login } from '../redux/auth';
-
-const mapActionsToProps = { login };
-
-export default connect(null, mapActionsToProps)(LoginForm)
