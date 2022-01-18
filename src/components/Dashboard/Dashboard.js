ede05fe6c03545b862120ebce2c8d5d528872921
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-01-18 04:53:59 +0100

    user connections and user invitations api requests added to dashboard
    user connections and user invitations api requests added to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,42 @@
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+
 import Navbar from '../Navbar/Navbar';
 import Sidebar from '../Sidebar/Sidebar';
 
+
 import './Dashboard.css'
 
-const Dashboard = () => (
-    <div className='dashboard'>
-        <Navbar />
-        <div className='dashboard__content'>
-            <Sidebar />
-        </div>
-    </div>
-)
+const Dashboard = () => {
+
+  const [connections, setConnections] = useState();
+  const [invitations, setInvitations] = useState();
+
+  useEffect(async () => {
+    const result = await axios.get(
+      'https://thc-ui-api.thrivecoin.com/v1/users/recent_connections',
+    );
+    setConnections(result.data);
+  },[]);
+
+  useEffect(async () => {
+    const result = await axios.get(
+      'https://thc-ui-api.thrivecoin.com/v1/users/invitations',
+    );
+    setInvitations(result.data);
+  },[]);
+
+    return (
+        (
+            <div className='dashboard'>
+                <Navbar />
+                <div className='dashboard__content'>
+                    <Sidebar />
+                </div>
+            </div>
+        )
+    )
+}
 
 export default Dashboard;
