c3410390a64bde8345358f25cb0de53c79d91fe8
Valmir Haxholli <valmir.haxholli@gmail.com>
2022-01-19 20:59:58 +0100

    Dashboard connections added based on connection type from the api
    Dashboard connections added based on connection type from the api

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Sidebar from '../Sidebar/Sidebar';
 import { Tabs, Tab, Content  } from '../Tab/Tab';
+import Button from '../Button/Button';
 
 
 import './Dashboard.css'
@@ -51,12 +52,48 @@ const Dashboard = () => {
                             </Tab>
                         </Tabs>
                         <>
-                            <Content active={active === 0}>
-                            <h1>Content 1</h1>
-                            </Content>
-                            <Content active={active === 1}>
-                            <h1>Content 2</h1>
-                            </Content>
+                        <Content active={active === 0}>
+                          <div className='connections'>
+                              <p>You have <span>{invitations?.data?.invitations.length} new connections</span></p>
+                          </div>
+                          <div className='connections__tab'>
+                            {invitations?.data?.invitations.filter(invitation => invitation.invitation_type === 'received').map(invitation => (
+                              <div  className='friends'>
+                              <div className='friends__details'>
+                              <img src={invitation?.image_url} alt="" />
+                                <div className='friends__info'>
+                                  <h3>{invitation?.name}</h3>
+                                  <p>{invitation?.title}</p>
+                                  <p>{invitation?.connections} connections</p>
+                                </div>
+                              </div>
+                              <p className='friends__msg'>{invitation?.message}</p>
+                              <div className='friends__btns'>
+                                <Button color='primary' name='Accept' />
+                                <Button color='secondary' name='Decline' />
+                              </div>
+                            </div>
+                            ))}
+                          </div>
+                        </Content>
+                        <Content active={active === 1}>
+                          <div className='connections__tab'>
+                            {invitations?.data?.invitations.filter(invitation => invitation.invitation_type === 'sent').map(invitation => (
+                              <div  className='friends'>
+                              <div className='friends__details'>
+
+                              <img src={invitation?.image_url} alt="" />
+                                <div className='friends__info'>
+                                  <h3>{invitation?.name}</h3>
+                                  <p>{invitation?.title}</p>
+                                  <p>{invitation?.connections} connections</p>
+                                </div>
+                              </div>
+                              <p className='friends__msg'>{invitation?.message}</p>
+                            </div>
+                            ))}
+                          </div>
+                        </Content>
                         </>
                     </div>
                 </div> 
